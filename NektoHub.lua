e87e9d729dbc22bfebaef2eebf7383fcfd28c9a6
werumZov <nikitazhelezhnechenko2011@gmail.com>
2025-03-02 19:56:20 +0300

    Update NektoHub.lua

--- NektoHub.lua
+++ NektoHub.lua
@@ -147,7 +147,7 @@ end
 
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "NektoHub(v1.2)", HidePremium = false, SaveConfig = true, ConfigFolder = "MineSim", IntroText = "Loaded!"})
+local Window = OrionLib:MakeWindow({Name = "NektoHub", HidePremium = false, SaveConfig = true, ConfigFolder = "MineSim", IntroText = "Loaded!"})
 
 
 local Tab = Window:MakeTab({Name = "Residence", Icon = "rbxassetid://4483345998", PremiumOnly = false })
