d77e8d085d4fdef8d62742974f95f3cc27b6f4d7
werumZov <nikitazhelezhnechenko2011@gmail.com>
2025-03-02 18:57:24 +0300

    Create ZovHub.lua

---
+++ ZovHub.lua
@@ -0,0 +1,59 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+--Zov Hub Test :D
+
+
+
+
+local Window = OrionLib:MakeWindow({Name = "Zov Hub(v0.2)", HidePremium = false, SaveConfig = true, ConfigFolder = "ZovTest", IntroText = "Zov Hub"})
+
+local PlayerTab = Window:MakeTab({
+	Name = "Player",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local AboutTab = Window:MakeTab({
+	Name = "About",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+
+local RandomTab = Window:MakeTab({
+	Name = "Random",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+RandomTab:AddButton({
+	Name = "Press me!",
+	Callback = function()
+      		print("button pressed")
+  	end    
+})
+
+AboutTab:AddButton({
+	Name = "Telegram",
+	Callback = function()
+      		print("t.me/TwoHubs")
+  	end    
+})
+
+
+
+
+
+local Section = PlayerTab:AddSection({
+	Name = "Movement"
+})
+
+PlayerTab:AddSlider({
+	Name = "WalkSpeed",
+	Min = 0,
+	Max = 20,
+	Default = 5,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	ValueName = "WS",
+	Callback = function(Value)
+		game.Player.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+	end    
+})
